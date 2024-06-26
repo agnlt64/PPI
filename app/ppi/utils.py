01516e6f0775d4c8ecb6231176f2a59f6f68550f
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2024-06-26 20:54:07 +0200

    fix encoding errors

--- app/ppi/utils.py
+++ app/ppi/utils.py
@@ -7,7 +7,8 @@ def read_source_file(filename: os.PathLike) -> str:
     """
     Retrieves the content of the `filename`. Used to parse a Python file.
     """
-    with open(filename, 'r') as source:
+    with open(filename, 'r', encoding='utf-8') as source:
+        print(source)
         return source.read()
 
 
@@ -16,13 +17,13 @@ def save_json(content: list, filename: str = JSON_OUTPUT) -> None:
     Save the `content` (Python dict) to the `filename`. By default, the filename is `JSON_OUTPUT`.
     """
     data = json.dumps(content, indent=4)
-    with open(filename, 'w') as out:
-        print(data, file=out)
+    with open(filename, 'w', encoding='utf-8') as out:
+        out.write(data)
 
 
 def read_json_file(filename: str = JSON_OUTPUT) -> dict:
     """
     Returns the content of the `filename` (must be a JSON file).
     """
-    with open(filename) as f:
+    with open(filename, encoding='utf-8') as f:
         return json.load(f)
